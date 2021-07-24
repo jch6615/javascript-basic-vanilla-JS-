626af7300a4ab1a49ecaf04e0ea3775c7f3cae88
jch6615 <77104502+jch6615@users.noreply.github.com>
2021-07-25 08:52:21 +0900

    #2.2

---
+++ MOMENTUM/script.js
@@ -0,0 +1,23 @@
+// magic-line
+// Console에 메세지를 보내는 magic-line
+// console.log(); 괄호안의 값이 console로 보내진다. (숫자, 문자 상관없음)
+
+// create variable
+// 1. const = constant 변화지 않는 값.
+const a = 10;
+const b = 2;
+
+const myName = "nico";
+
+// Variable의 이름을 정하는 규칙. 요령.
+// 1. variable의 이름에는 공백이 들어갈 수 없다.
+// 2. 여러 단어를 붙어 이름을 만들때는 camel case를 이용한다.
+// ex) veryLongVariableName
+
+// tips) Python 에서는 camel case가 아니라 단어마다 _를 넣는다.
+// ex) very_long_variable_name
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
