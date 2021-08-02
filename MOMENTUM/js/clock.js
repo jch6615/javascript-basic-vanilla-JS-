337f217b426b959555dd69e78166ba24feccab51
jch6615 <77104502+jch6615@users.noreply.github.com>
2021-08-02 12:04:25 +0900

    #5.1Timeouts and Dates

--- MOMENTUM/js/clock.js
+++ MOMENTUM/js/clock.js
@@ -1,15 +1,10 @@
 const clock = document.querySelector("#clock");
 
-//🌟 intervals & timeout 🌟
-function sayHello() {
-    console.log("hello");
+//🌟 clock 🌟
+function getClock() {
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 }
-//sayHello()를 2초마다 실행시켜보자.
 
-/*
-🌟 setInterval(a,b) 
-setInterval은 두개의 argument를 가진다.
-a: 실행하고 싶은 처리, 함수
-b: 호출되는 function의 간격. 몇 ms(milliseconds)으로 할지.
-*/
-setInterval(sayHello, 5000); //5000ms === 5s 
+getClock()
+setInterval(getClock, 1000);
