7b49d832210b58bc2a5e1ec66574f88aeb0fd011
jch6615 <77104502+jch6615@users.noreply.github.com>
2021-08-06 08:22:05 +0900

    #7.0 Setup

---
+++ MOMENTUM/js/todo.js
@@ -0,0 +1,12 @@
+const toDoForm = document.querySelector("#todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.querySelector("#todo-list");
+
+
+function handleToDosubmit(event) {
+    event.preventDefault();
+    const newToDo = toDoInput.value;
+    toDoInput.value = "";
+}
+
+toDoForm.addEventListener("submit", handleToDosubmit);
