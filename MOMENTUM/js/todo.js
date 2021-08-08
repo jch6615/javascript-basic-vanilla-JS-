60556476acf920c5ee7d3c76e8c23c4306b87638
jch6615 <77104502+jch6615@users.noreply.github.com>
2021-08-08 14:40:32 +0900

    #7.5 Loading To Dos Part Two

--- MOMENTUM/js/todo.js
+++ MOMENTUM/js/todo.js
@@ -4,7 +4,7 @@ const toDoList = document.querySelector("#todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 //todo 내용을 입력하면 내용을 저장할 Array를 만든다.
 //❗️ localStorage에서는 Array를 저장 할 수 없다. 오직 텍스트만 저장 가능.
 
@@ -50,14 +50,10 @@ function handleToDosubmit(event) {
 
 toDoForm.addEventListener("submit", handleToDosubmit);
 
-function sayHello(item) {
-    console.log("this is the turn of", item);
-}
-
 const savedToDos = localStorage.getItem(TODOS_KEY);
 console.log(savedToDos);
 if(savedToDos){
     const parsedToDos = JSON.parse(savedToDos);
-    //parsedToDos.forEach(sayHello);
-    parsedToDos.forEach(element => console.log("this is the turn of ", element));
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
 }
